3c276f21e8b70d760c5a0cd8c04162a7e3014584
Rustam Singh Tomer <91615721+RustamTomer@users.noreply.github.com>
2025-06-12 19:38:34 +0530

    Update Navbar.jsx
    navbar updated

--- passop/src/components/Navbar.jsx
+++ passop/src/components/Navbar.jsx
@@ -19,11 +19,6 @@ const Navbar = () => {
                         <a className='hover:font-bold' href='#'>Contact</a>
                     </li>
                 </ul> */}
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
-                    <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span>
-                    
-                </button>
             </div>
         </nav>
     )
